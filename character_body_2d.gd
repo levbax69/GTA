f30cdbfc708e6bbd40cf14540b21b8da0e0c1a54
levbax69 <levbax69@kingshigh.school.nz>
2025-06-27 09:39:08 +1200

    v1.0.1.9
    Added stuff, stuff didn't work. I crashed out.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @export var movement_speed : float = 500
 var character_direction : Vector2
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	character_direction.x = Input.get_axis("a", "d")
 	character_direction.y = Input.get_axis("w", "s")
 	
