e12895568d5ebf06e8b6ecf617eb227610f3c11c
levbax69 <levbax69@kingshigh.school.nz>
2025-07-29 09:59:23 +1200

    v1.0.2.1
    Error with code when clicking button. Deleted script causing issue.

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -8,14 +8,5 @@ func _physics_process(_delta):
 	character_direction.y = Input.get_axis("w", "s")
 	
 	#flip
-	if character_direction.x > 0: %sprite.flip_h = false
-	elif character_direction.y < 0: %sprite.flip_h = true
-	
-	if character_direction: 
-		velocity = character_direction * movement_speed
-		if %sprite.animation != "Walking": %sprite.animation = "Walking"
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
-		if %sprite.animation != "Idle": %sprite.animation = "Idle"
 		
 	move_and_slide()
