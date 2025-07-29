ad294b4779f835e473cda400b5c4234d19ab39eb
levbax69 <levbax69@kingshigh.school.nz>
2025-07-30 10:41:12 +1200

    1.0.3
    Fixed movement

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -3,10 +3,20 @@ extends CharacterBody2D
 @export var movement_speed : float = 500
 var character_direction : Vector2
 
-func _physics_process(_delta):
-	character_direction.x = Input.get_axis("a", "d")
-	character_direction.y = Input.get_axis("w", "s")
-	
+func _physics_process(delta):
+	character_direction .x = Input.get_axis("a","d")
+	character_direction .y = Input.get_axis("w","s")
+   
 	#flip
-		
+	if character_direction.x > 0: $sprite.flip_h = false
+	elif  character_direction.x > 0: $sprite.flip_h = true
+   
+	if character_direction:
+		velocity = character_direction * movement_speed
+		if $sprite.animation != "walking": $sprite.animation = "walking"
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
+		if $sprite.animation != "idle":
+			$sprite.animation = "idle"
+	   
 	move_and_slide()
